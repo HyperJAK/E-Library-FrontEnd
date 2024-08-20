1d0d3106208c88f7c1981262eec6ce605ca38bd8
HyperJAK <james.jakthesamething@gmail.com>
2024-08-20 15:25:50 +0300

    Basic working project blueprint

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 'use client'
 import {useState} from "react";
 import {fetchAndFormatBooks} from "@/config/API/book/bookService";
+import RecipeSearch from "@/components/home/RecipeSearch";
+import InterestingInfo from "@/components/home/InterestingInfo";
+
 
 export default function Home() {
 
@@ -18,28 +21,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center justify-center min-h-screen p-4">
-        <button
-            onClick={handleFetchBooks}
-            className="px-6 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600"
-        >
-          Fetch Books
-        </button>
+      {/*All home page features*/}
 
-        {books.length > 0 && (
-            <div className="mt-8 w-full max-w-4xl">
-              <h2 className="text-2xl font-semibold">Book List:</h2>
-              <ul className="mt-4">
-                {books.map((book) => (
-                    <li key={book.id} className="mb-2 p-2 border rounded">
-                      <h3 className="font-bold">{book.title}</h3>
-                      <p>Published Date: {book.formattedDate}</p>
-                    </li>
-                ))}
-              </ul>
-            </div>
-        )}
-      </div>
+      <RecipeSearch />
+      <InterestingInfo />
     </main>
   );
 }
